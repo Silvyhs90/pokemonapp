2461241cd42848419647e6e5c9ba838d70d1b57f
Silvina Lorena <silvyhs.campanitah@hotmail.com>
2021-05-12 03:09:16 -0300

    arregle el pages/pokeCard.js el src

--- src/pages/PokeCard/PokeCard.js
+++ src/pages/PokeCard/PokeCard.js
@@ -3,20 +3,19 @@ import React from "react";
 /* aca se reciben las propiedades, los props */
 export const PokeCard = ({ pokemon }) => {   
     const [pokemonData, setPokemonData] = React.useState();
-
+               
        React.useEffect(() => {
             fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((response) =>
              response.json().then((data) => setPokemonData(data))
              );
              
-          }, []);
-    
+          }, [ pokemon ]);
+       
         return (
             <>
             {pokemonData && (
-             <img scr= {pokemonData.sprites.front_default} alt ="Imagen del Pokemon" />
+             <img src={pokemonData.sprites.front_default} alt="Imagen del Pokemon" />
             )}
             </>
         );
-
     };
